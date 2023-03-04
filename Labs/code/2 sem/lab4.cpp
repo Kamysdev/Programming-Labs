ffe4be30dffb16cc9ef85f05e548da06c0fe26b5
Kamysdev <work.kamysdev@gmail.com>
2023-03-04 15:19:36 +0700

    Work with lab4

--- Labs/code/2 sem/lab4.cpp
+++ Labs/code/2 sem/lab4.cpp
@@ -3,92 +3,92 @@
 #include <iostream>
 #include <time.h>
 
-using namespace std;
-
-const int n = 5;
-
-void max_search_n_delete(int** matrix, int m)
-{
-	int maxi = 0, maxi_i, maxi_j;
-
-	for (int i = 0; i < m; i++)
-	{
-		for (int j = 0; j < n; j++)
-		{
-			if (matrix[i][j] > maxi)
-			{
-				maxi = matrix[i][j];
-				maxi_i = i;
-				maxi_j = j;
-			}
-		}
-	}
-	
-	int** new_matrix = new int* [m-1];
-
-	for (int i = 0; i < m - 1; i++)
-	{
-		new_matrix[i] = new int[m - 1];
-	}
-
-	for (int i = 0, i1 = 0; i < m, i1 < m - 1; i++, i1++)
-	{
-		if (i == maxi_i)
-		{
-			i++;
-		}
-
-		for ( int j = 0, j1 = 0; j < n, j1 < n - 1; j++, j1++)
-		{
-			if (j == maxi_j)
-			{
-				j++;
-			}
-
-			new_matrix[i1][j1] = matrix[i][j];
-
-			printf("%d ", new_matrix[i1][j1]);
-		}
-
-		printf("\n");
-	}
-
-	new_matrix = NULL;
-}
-
-int main()
-{
-	int i, j, m;
-	srand(time(NULL));
-
-	printf("enter amount of lines ");
-	scanf_s("%d", &m);
-
-	int** matrix = new int* [m];
-
-	for (i = 0; i < m; i++)
-	{
-		matrix[i] = new int[m];
-	}
-
-	for (i = 0; i < m; i++)
-	{
-		for (j = 0; j < n; j++)
-		{
-			matrix[i][j] = rand() % 100;
-			printf("%d ", matrix[i][j]);
-		}
-		
-		printf("\n");
-	}
-
-	printf("\n");
-
-	max_search_n_delete(matrix, m);
-
-	matrix = NULL;
-	return 0;
-}
+//using namespace std;
+//
+//const int n = 5;
+//
+//void max_search_n_delete(int** matrix, int m)
+//{
+//	int maxi = 0, maxi_i, maxi_j;
+//
+//	for (int i = 0; i < m; i++)
+//	{
+//		for (int j = 0; j < n; j++)
+//		{
+//			if (matrix[i][j] > maxi)
+//			{
+//				maxi = matrix[i][j];
+//				maxi_i = i;
+//				maxi_j = j;
+//			}
+//		}
+//	}
+//	
+//	int** new_matrix = new int* [m-1];
+//
+//	for (int i = 0; i < m - 1; i++)
+//	{
+//		new_matrix[i] = new int[m - 1];
+//	}
+//
+//	for (int i = 0, i1 = 0; i < m, i1 < m - 1; i++, i1++)
+//	{
+//		if (i == maxi_i)
+//		{
+//			i++;
+//		}
+//
+//		for ( int j = 0, j1 = 0; j < n, j1 < n - 1; j++, j1++)
+//		{
+//			if (j == maxi_j)
+//			{
+//				j++;
+//			}
+//
+//			new_matrix[i1][j1] = matrix[i][j];
+//
+//			printf("%d ", new_matrix[i1][j1]);
+//		}
+//
+//		printf("\n");
+//	}
+//
+//	new_matrix = NULL;
+//}
+//
+//int main()
+//{
+//	int i, j, m;
+//	srand(time(NULL));
+//
+//	printf("enter amount of lines ");
+//	scanf_s("%d", &m);
+//
+//	int** matrix = new int* [m];
+//
+//	for (i = 0; i < m; i++)
+//	{
+//		matrix[i] = new int[m];
+//	}
+//
+//	for (i = 0; i < m; i++)
+//	{
+//		for (j = 0; j < n; j++)
+//		{
+//			matrix[i][j] = rand() % 100;
+//			printf("%d ", matrix[i][j]);
+//		}
+//		
+//		printf("\n");
+//	}
+//
+//	printf("\n");
+//
+//	max_search_n_delete(matrix, m);
+//
+//	matrix = NULL;
+//	return 0;
+//}
 //
 // 
 // 
