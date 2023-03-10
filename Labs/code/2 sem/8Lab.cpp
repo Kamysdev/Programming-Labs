6e6a6adf722eaff52b7ff18808a68d045df107e0
KamysDev <work.kamysdev@gmail.com>
2023-03-10 15:17:27 +0700

    Coding Lab8

--- Labs/code/2 sem/8Lab.cpp
+++ Labs/code/2 sem/8Lab.cpp
@@ -82,99 +82,99 @@
 //	return 0;
 //}
 
-//#define _CRT_SECURE_NO_WARNINGS
-//#include <stdlib.h>
-//#include <stdio.h>
-//#include <string.h>
-//
-//typedef struct
-//{
-//	int roomNumb;
-//	float square;
-//	char facultName[180];
-//	int livingIn;
-//} dormitory;
-//
-//int FacultCountAndWrite(dormitory *arr, int size, bool IsWrite, char* index)		// Счётчик уникальных элементов char
-//{
-//	int flag = 0, tempcount = 0;
-//
-//	for (int i = 0; i < size; i++)
-//	{
-//		bool isUnique = false;
-//
-//		for (int j = i + 1; j < size; j++)
-//		{
-//			if ((arr[i].facultName != arr[j].facultName))
-//			{
-//				isUnique = true;
-//			}
-//		}
-//		if (isUnique)
-//		{
-//			if (IsWrite)
-//			{
-//				strcpy(&index[0], arr[0].facultName);
-//
-//				strcpy(&index[tempcount], arr[i].facultName);
-//				strcpy(&index[tempcount + strlen(arr[i].facultName)], " ");
-//				tempcount += strlen(arr[i].facultName) + 1;
-//			}
-//			flag += strlen(arr[i].facultName) + 1;
-//		}
-//		if (size == 1)
-//		{
-//			flag += strlen(arr[i].facultName) + 1;
-//			if (IsWrite)
-//			{
-//				strcpy(&index[0], arr[0].facultName);
-//			}
-//		}
-//	}
-//
-//	return flag;
-//}
-//
-//int main()
-//{
-//	int size;
-//
-//	scanf_s("%d", &size);
-//	dormitory *arr = new dormitory[size];
-//
-//	for (int i = 0; i < size; i++)
-//	{
-//		printf("\nRoom number: ");
-//		scanf_s("%d", &arr[i].roomNumb);
-//		printf("Square: ");
-//		scanf_s("%f", &arr[i].square);
-//		printf("Name of Facultet: ");
-//		scanf_s("%s", arr[i].facultName, 10);
-//		printf("How many people live in: ");
-//		scanf_s("%d", &arr[i].livingIn);
-//	}
-//
-//	int ** square_counter = new  int* [size];
-//	
-//	//for (int i = 0; i < size; i++)
-//	//{
-//	//	square_counter[i] = new  int[4];
-//	//
-//	//	for (int j = 0; j < 4; j++)
-//	//	{
-//	//		square_counter[i][j] = 2;
-//	//		printf("%d ", square_counter[i][j]);
-//	//	}
-//	//	printf("\n");
-//	//}
-//
-//	char* facult = new char [FacultCountAndWrite(arr, size, NULL, NULL)];
-//	int IsComplited = FacultCountAndWrite(arr, size, true, facult);
-//
-//	for (int i = 0; i < size; i++)
-//	{ }
-//
-//	printf(facult);
-//
-//	return 0;
-//}
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+typedef struct
+{
+	int roomNumb;
+	float square;
+	char facultName[180];
+	int livingIn;
+} dormitory;
+
+int FacultCountAndWrite(dormitory *arr, int size, bool IsWrite, char* index)		// Счётчик уникальных элементов char
+{
+	int flag = 0, tempcount = 0;
+
+	for (int i = 0; i < size; i++)
+	{
+		bool isUnique = true;
+
+		for (int j = i + 1; j < size; j++)
+		{
+			if ((arr[i].facultName == arr[j].facultName))
+			{
+				isUnique = false;
+			}
+		}
+		if (isUnique)
+		{
+			if (IsWrite)
+			{
+				// strcpy(&index[0], arr[0].facultName);							// Зачем я это написал 
+
+				strcpy(&index[tempcount], arr[i].facultName);
+				strcpy(&index[tempcount + strlen(arr[i].facultName)], " ");
+				tempcount += strlen(arr[i].facultName) + 1;
+			}
+			flag += strlen(arr[i].facultName) + 1;
+		}
+		if (size == 1)
+		{
+			flag += strlen(arr[i].facultName) + 1;
+			if (IsWrite)
+			{
+				strcpy(&index[0], arr[0].facultName);
+			}
+		}
+	}
+
+	return flag;
+}
+
+int main()
+{
+	int size;
+
+	scanf_s("%d", &size);
+	dormitory *arr = new dormitory[size];
+
+	for (int i = 0; i < size; i++)
+	{
+		printf("\nRoom number: ");
+		scanf_s("%d", &arr[i].roomNumb);
+		printf("Square: ");
+		scanf_s("%f", &arr[i].square);
+		printf("Name of Facultet: ");
+		scanf_s("%s", arr[i].facultName, 10);
+		printf("How many people live in: ");
+		scanf_s("%d", &arr[i].livingIn);
+	}
+
+	int ** square_counter = new  int* [size];
+	
+	//for (int i = 0; i < size; i++)
+	//{
+	//	square_counter[i] = new  int[4];
+	//
+	//	for (int j = 0; j < 4; j++)
+	//	{
+	//		square_counter[i][j] = 2;
+	//		printf("%d ", square_counter[i][j]);
+	//	}
+	//	printf("\n");
+	//}
+
+	char* facult = new char [FacultCountAndWrite(arr, size, NULL, NULL)];
+	int IsComplited = FacultCountAndWrite(arr, size, true, facult);
+
+	for (int i = 0; i < size; i++)
+	{ }
+
+	printf(facult);
+
+	return 0;
+}
