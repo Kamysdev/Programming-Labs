1b15ae7bafa1fef62805cb44027b8792cbadf6aa
Bell404ll <Bell.4004.ll@gmail.com>
2023-04-07 15:05:35 +0700

    Edited lab8

--- Labs/code/2 sem/9Lab.cpp
+++ Labs/code/2 sem/9Lab.cpp
@@ -1,94 +1,112 @@
-#include <iostream>
-#include <string>
-using namespace std;
-
-// Структура для хранения информации о студенте
-struct Student {
-    string surname;
-    int marks[4];
-    Student* next;
-};
-
-// Функция для добавления нового элемента в список
-void add(Student*& head, string surname, int marks[]) {
-    // Создание нового элемента
-    Student* new_student = new Student;
-    new_student->surname = surname;
-    for (int i = 0; i < 4; i++) {
-        new_student->marks[i] = marks[i];
-    }
-    new_student->next = NULL;
-    // Добавление элемента в список
-    if (head == NULL) {
-        head = new_student;
-    }
-    else {
-        Student* current = head;
-        while (current->next != NULL) {
-            current = current->next;
-        }
-        current->next = new_student;
-    }
-}
-
-// Функция для сортировки списка по фамилии студента
-void sort(Student*& head) {
-    if (head == NULL || head->next == NULL) {
-        return;
-    }
-    Student* current = head;
-    while (current != NULL) {
-        Student* min = current;
-        Student* temp = current->next;
-        while (temp != NULL) {
-            if (temp->surname < min->surname) {
-                min = temp;
-            }
-            temp = temp->next;
-        }
-        // Обмен элементов
-        string temp_surname = current->surname;
-        for (int i = 0; i < 4; i++) {
-            int temp_mark = current->marks[i];
-            current->marks[i] = min->marks[i];
-            min->marks[i] = temp_mark;
-        }
-        current->surname = min->surname;
-        min->surname = temp_surname;
-        current = current->next;
-    }
-}
-
-// Функция для вывода списка
-void print(Student* head) {
-    if (head == NULL) {
-        cout << "Список пуст" << endl;
-        return;
-    }
-    cout << "Список студентов:" << endl;
-    while (head != NULL) {
-        cout << head->surname << ": ";
-        for (int i = 0; i < 4; i++) {
-            cout << head->marks[i] << " ";
-        }
-        cout << endl;
-        head = head->next;
-    }
-}
-
-int main() {
-
-    setlocale(LC_ALL, "Rus");
-
-    Student* head = NULL;
-    // Добавление элементов в список
-    add(head, "Иванов", new int[4] {5, 4, 5, 4});
-    add(head, "Петров", new int[4] {3, 3, 4, 5});
-    add(head, "Сидоров", new int[4] {4, 5, 4, 5});
-    add(head, "Козлов", new int[4] {5, 5, 5, 5});
-    // Сортировка списка
-    sort(head);
-    // Вывод списка
-    print(head);
-    return 0;
-}
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//struct Student 
+//{
+//    string surname;
+//    int marks[4];
+//    Student* next;
+//};
+//
+//void add(Student*& head, string surname, int* marks) 
+//{
+//    Student* new_student = new Student;
+//    new_student->surname = surname;
+//
+//    for (int i = 0; i < 4; i++) 
+//    {
+//        new_student->marks[i] = marks[i];
+//    }
+//    new_student->next = NULL;
+//
+//    if (head == NULL) 
+//    {
+//        head = new_student;
+//    }
+//    else 
+//    {
+//        Student* current = head;
+//
+//        while (current->next != NULL) 
+//        {
+//            current = current->next;
+//        }
+//
+//        current->next = new_student;
+//    }
+//}
+//
+//void sort(Student*& head) 
+//{
+//    if (head == NULL || head->next == NULL) 
+//    {
+//        return;
+//    }
+//
+//    Student* current = head;
+//
+//    while (current != NULL) 
+//    {
+//        Student* min = current;
+//        Student* temp = current->next;
+//
+//        while (temp != NULL) 
+//        {
+//            if (temp->surname < min->surname) 
+//            {
+//                min = temp;
+//            }
+//            temp = temp->next;
+//        }
+//        string temp_surname = current->surname;
+//        for (int i = 0; i < 4; i++) {
+//            int temp_mark = current->marks[i];
+//            current->marks[i] = min->marks[i];
+//            min->marks[i] = temp_mark;
+//        }
+//        current->surname = min->surname;
+//        min->surname = temp_surname;
+//        current = current->next;
+//    }
+//}
+//
+//void print(Student* head) 
+//{
+//    if (head == NULL) 
+//    {
+//        cout << "Список пуст" << endl;
+//        return;
+//    }
+//    cout << "Список студентов:" << endl;
+//    while (head != NULL) 
+//    {
+//        cout << head->surname << ": ";
+//
+//        for (int i = 0; i < 4; i++) 
+//        {
+//            cout << head->marks[i] << " ";
+//        }
+//
+//        cout << endl;
+//        head = head->next;
+//    }
+//}
+//
+//int main() 
+//{
+//
+//    setlocale(LC_ALL, "Rus");
+//
+//    Student* head = NULL;
+//
+//    add(head, "Иванов", new int[4] {5, 4, 5, 4});
+//    add(head, "Петров", new int[4] {3, 3, 4, 5});
+//    add(head, "Сидоров", new int[4] {4, 5, 4, 5});
+//    add(head, "Козлов", new int[4] {5, 5, 5, 5});
+//
+//    sort(head);
+//    print(head);
+//
+//    return 0;
+//}
--- Labs/code/SAOD/Lab9Saod.cpp
+++ Labs/code/SAOD/Lab9Saod.cpp
@@ -1,157 +1,157 @@
-#include <iostream>
-
-using namespace std;
-
-void FillInc(int* x, int n) {
-    for (int i = 0; i < n; i++) {
-        x[i] = i + 1;
-    }
-}
-
-void FillDec(int* x, int n) {
-    for (int i = 0; i < n; i++) {
-        x[i] = n - i;
-    }
-}
-
-void FillRand(int* x, int n) {
-    srand(time(0));
-    for (int i = 0; i < n; i++) {
-        x[i] = rand() % 10000 + 1;
-    }
-}
-
-int CheckSum(int* a, int size) {
-    int sum = 0;
-    for (int i = 0; i < size; i++)
-        sum += a[i];
-    return sum;
-}
-
-int RunNumber(int* a, int size) {
-    int count = 1;
-    for (int i = 1; i < size; i++) {
-        count = (a[i] < a[i - 1]) ? count + 1 : count;
-    }
-    return count;
-}
-
-void heapify(int arr[], int n, int i, int& flag1, int& flag2) {
-    int largest = i;
-    int left = 2 * i + 1;
-    int right = 2 * i + 2;
-
-    if (left < n && arr[left] > arr[largest]) {
-        flag1++;
-        largest = left;
-    }
-
-    if (right < n && arr[right] > arr[largest]) {
-        flag1++;
-        largest = right;
-    }
-
-    if (largest != i) {
-        swap(arr[i], arr[largest]);
-        flag2 += 3;
-        heapify(arr, n, largest, flag1, flag2);
-    }
-}
-
-void heapSort(int arr[], int n, int& flag1, int& flag2) {
-    flag1 = 0; flag2 = 0;
-    for (int i = n / 2 - 1; i >= 0; i--)
-        heapify(arr, n, i, flag1, flag2);
-    for (int i = n - 1; i >= 0; i--) {
-        flag2 += 3;
-        swap(arr[0], arr[i]);
-        heapify(arr, i, 0, flag1, flag2);
-    }
-}
-
-void printArray(int arr[], int n) {
-    for (int i = 0; i < n; ++i)
-        cout << arr[i] << " ";
-    cout << "\n";
-}
-
-int main() {
-    int n, M, C, M1, C1, M2, C2;
-
-    n = 100;
-    int* arr = new int[n];
-    FillRand(arr, n);
-    printArray(arr, n);
-    printf("\nControl Summ : %d", CheckSum(arr, n));
-    printf("\nAmount of series : %d", RunNumber(arr, n));
-    printf("\n\n");
-    heapSort(arr, n, C, M);
-    printArray(arr, n);
-    printf("\nControl Summ : %d", CheckSum(arr, n));
-    printf("\nAmount of series : %d", RunNumber(arr, n));
-
-    FillInc(arr, n);
-    heapSort(arr, n, C, M);
-    FillDec(arr, n);
-    heapSort(arr, n, C1, M1);
-    FillRand(arr, n);
-    heapSort(arr, n, C2, M2);
-
-    printf("\n-------------------------------------------------------------------------------");
-    printf("\n    n   |\t\t\t    HeapSort(M + S)\t\t              |");
-    printf("\n        -----------------------------------------------------------------------");
-    printf("\n\t|\t  inc\t      |\t\tdec\t      |\t\trad\t      |");
-    printf("\n-------------------------------------------------------------------------------");
-    printf("\n   100  |\t %d \t      |\t\t%d\t      |\t\t%d\t      |", M + C, M1 + C1, M2 + C2);
-    printf("\n-------------------------------------------------------------------------------");
-
-    n = 200;
-    arr = new int[n];
-    FillInc(arr, n);
-    heapSort(arr, n, C, M);
-    FillDec(arr, n);
-    heapSort(arr, n, C1, M1);
-    FillRand(arr, n);
-    heapSort(arr, n, C2, M2);
-
-    printf("\n   200  |\t %d \t      |\t\t%d\t      |\t\t%d\t      |", M + C, M1 + C1, M2 + C2);
-    printf("\n-------------------------------------------------------------------------------");
-
-    n = 300;
-    arr = new int[n];
-    FillInc(arr, n);
-    heapSort(arr, n, C, M);
-    FillDec(arr, n);
-    heapSort(arr, n, C1, M1);
-    FillRand(arr, n);
-    heapSort(arr, n, C2, M2);
-
-    printf("\n   300  |\t %d \t      |\t\t%d\t      |\t\t%d\t      |", M + C, M1 + C1, M2 + C2);
-    printf("\n-------------------------------------------------------------------------------");
-
-    n = 400;
-    arr = new int[n];
-    FillInc(arr, n);
-    heapSort(arr, n, C, M);
-    FillDec(arr, n);
-    heapSort(arr, n, C1, M1);
-    FillRand(arr, n);
-    heapSort(arr, n, C2, M2);
-
-    printf("\n   400  |\t %d \t      |\t\t%d\t      |\t\t%d\t      |", M + C, M1 + C1, M2 + C2);
-    printf("\n-------------------------------------------------------------------------------");
-
-    n = 500;
-    arr = new int[n];
-    FillInc(arr, n);
-    heapSort(arr, n, C, M);
-    FillDec(arr, n);
-    heapSort(arr, n, C1, M1);
-    FillRand(arr, n);
-    heapSort(arr, n, C2, M2);
-
-    printf("\n   500  |\t %d \t      |\t\t%d\t      |\t\t%d\t      |", M + C, M1 + C1, M2 + C2);
-    printf("\n-------------------------------------------------------------------------------");
-
-    cout << endl;
-}
+//#include <iostream>
+//
+//using namespace std;
+//
+//void FillInc(int* x, int n) {
+//    for (int i = 0; i < n; i++) {
+//        x[i] = i + 1;
+//    }
+//}
+//
+//void FillDec(int* x, int n) {
+//    for (int i = 0; i < n; i++) {
+//        x[i] = n - i;
+//    }
+//}
+//
+//void FillRand(int* x, int n) {
+//    srand(time(0));
+//    for (int i = 0; i < n; i++) {
+//        x[i] = rand() % 10000 + 1;
+//    }
+//}
+//
+//int CheckSum(int* a, int size) {
+//    int sum = 0;
+//    for (int i = 0; i < size; i++)
+//        sum += a[i];
+//    return sum;
+//}
+//
+//int RunNumber(int* a, int size) {
+//    int count = 1;
+//    for (int i = 1; i < size; i++) {
+//        count = (a[i] < a[i - 1]) ? count + 1 : count;
+//    }
+//    return count;
+//}
+//
+//void heapify(int arr[], int n, int i, int& flag1, int& flag2) {
+//    int largest = i;
+//    int left = 2 * i + 1;
+//    int right = 2 * i + 2;
+//
+//    if (left < n && arr[left] > arr[largest]) {
+//        flag1++;
+//        largest = left;
+//    }
+//
+//    if (right < n && arr[right] > arr[largest]) {
+//        flag1++;
+//        largest = right;
+//    }
+//
+//    if (largest != i) {
+//        swap(arr[i], arr[largest]);
+//        flag2 += 3;
+//        heapify(arr, n, largest, flag1, flag2);
+//    }
+//}
+//
+//void heapSort(int arr[], int n, int& flag1, int& flag2) {
+//    flag1 = 0; flag2 = 0;
+//    for (int i = n / 2 - 1; i >= 0; i--)
+//        heapify(arr, n, i, flag1, flag2);
+//    for (int i = n - 1; i >= 0; i--) {
+//        flag2 += 3;
+//        swap(arr[0], arr[i]);
+//        heapify(arr, i, 0, flag1, flag2);
+//    }
+//}
+//
+//void printArray(int arr[], int n) {
+//    for (int i = 0; i < n; ++i)
+//        cout << arr[i] << " ";
+//    cout << "\n";
+//}
+//
+//int main() {
+//    int n, M, C, M1, C1, M2, C2;
+//
+//    n = 100;
+//    int* arr = new int[n];
+//    FillRand(arr, n);
+//    printArray(arr, n);
+//    printf("\nControl Summ : %d", CheckSum(arr, n));
+//    printf("\nAmount of series : %d", RunNumber(arr, n));
+//    printf("\n\n");
+//    heapSort(arr, n, C, M);
+//    printArray(arr, n);
+//    printf("\nControl Summ : %d", CheckSum(arr, n));
+//    printf("\nAmount of series : %d", RunNumber(arr, n));
+//
+//    FillInc(arr, n);
+//    heapSort(arr, n, C, M);
+//    FillDec(arr, n);
+//    heapSort(arr, n, C1, M1);
+//    FillRand(arr, n);
+//    heapSort(arr, n, C2, M2);
+//
+//    printf("\n-------------------------------------------------------------------------------");
+//    printf("\n    n   |\t\t\t    HeapSort(M + S)\t\t              |");
+//    printf("\n        -----------------------------------------------------------------------");
+//    printf("\n\t|\t  inc\t      |\t\tdec\t      |\t\trad\t      |");
+//    printf("\n-------------------------------------------------------------------------------");
+//    printf("\n   100  |\t %d \t      |\t\t%d\t      |\t\t%d\t      |", M + C, M1 + C1, M2 + C2);
+//    printf("\n-------------------------------------------------------------------------------");
+//
+//    n = 200;
+//    arr = new int[n];
+//    FillInc(arr, n);
+//    heapSort(arr, n, C, M);
+//    FillDec(arr, n);
+//    heapSort(arr, n, C1, M1);
+//    FillRand(arr, n);
+//    heapSort(arr, n, C2, M2);
+//
+//    printf("\n   200  |\t %d \t      |\t\t%d\t      |\t\t%d\t      |", M + C, M1 + C1, M2 + C2);
+//    printf("\n-------------------------------------------------------------------------------");
+//
+//    n = 300;
+//    arr = new int[n];
+//    FillInc(arr, n);
+//    heapSort(arr, n, C, M);
+//    FillDec(arr, n);
+//    heapSort(arr, n, C1, M1);
+//    FillRand(arr, n);
+//    heapSort(arr, n, C2, M2);
+//
+//    printf("\n   300  |\t %d \t      |\t\t%d\t      |\t\t%d\t      |", M + C, M1 + C1, M2 + C2);
+//    printf("\n-------------------------------------------------------------------------------");
+//
+//    n = 400;
+//    arr = new int[n];
+//    FillInc(arr, n);
+//    heapSort(arr, n, C, M);
+//    FillDec(arr, n);
+//    heapSort(arr, n, C1, M1);
+//    FillRand(arr, n);
+//    heapSort(arr, n, C2, M2);
+//
+//    printf("\n   400  |\t %d \t      |\t\t%d\t      |\t\t%d\t      |", M + C, M1 + C1, M2 + C2);
+//    printf("\n-------------------------------------------------------------------------------");
+//
+//    n = 500;
+//    arr = new int[n];
+//    FillInc(arr, n);
+//    heapSort(arr, n, C, M);
+//    FillDec(arr, n);
+//    heapSort(arr, n, C1, M1);
+//    FillRand(arr, n);
+//    heapSort(arr, n, C2, M2);
+//
+//    printf("\n   500  |\t %d \t      |\t\t%d\t      |\t\t%d\t      |", M + C, M1 + C1, M2 + C2);
+//    printf("\n-------------------------------------------------------------------------------");
+//
+//    cout << endl;
+//}
