b3511289d6eeb3201a8648840d4c48bc0df77466
KamysDev <sereginka070504@gmail.com>
2023-05-15 14:03:05 +0700

    Added digital sort

--- Labs/code/2 sem/8Lab.cpp
+++ Labs/code/2 sem/8Lab.cpp
@@ -82,87 +82,87 @@
 //	return 0;
 //}
 
-#include <iostream>
-#include <string>
-
-struct dormitory
-{
-	int roomNumb;
-	float square;
-	std::string facultName;
-	int livingIn;
-};
-
-void FacultCountAndWrite(dormitory *arr, int size, std::string* index)		// —чЄтчик уникальных элементов string
-{
-	bool unique;
-
-	for (int i = 0, count = 0; i < size; i++)
-	{
-		unique = true;
-		for (int j = 0; j < index->size(); j++)
-		{
-			if (index[j] == arr[i].facultName)
-			{
-				unique = false;
-			}
-		}
-
-		if (unique)
-		{
-			index[count] = arr[i].facultName;
-			count++;
-		}
-	}
-}
-
-int main()
-{
-	int size;
-
-	std::cin >> size;
-	dormitory *arr = new dormitory[size];
-
-	for (int i = 0; i < size; i++)
-	{
-		std::cout << "Room number: ";
-		std::cin >> arr[i].roomNumb;
-		std::cout << "Square: ";
-		std::cin >> arr[i].square;
-		std::cout << "Name of Facultet: ";
-		std::cin >> arr[i].facultName;
-		std::cout << "How many people live in: ";
-		std::cin >> arr[i].livingIn;
-	}
-
-	std::string* facult = new std::string[size];			// сделать массивом :)
-	FacultCountAndWrite(arr, size, facult);
-
-	int roomcount = 0, studentscount = 0;
-	float midsquare = 0;
-	for (int i = 0; i < size; i++)
-	{ 
-		std::cout << "Name of facult: " << facult[i] << std::endl;
-		for (int j = 0; j < size; j++)
-		{
-			if (arr[j].facultName == facult[i])
-			{
-				roomcount++;
-				studentscount += arr[j].livingIn;
-				midsquare += arr[j].square;
-			}
-		}
-
-		midsquare /= studentscount;
-
-		std::cout << "No of rooms: " << roomcount 
-			<< "  No of students: " << studentscount 
-			<< "  Mid square: " << midsquare << std::endl << std::endl;
-
-		roomcount = 0;
-		studentscount = 0;
-		midsquare = 0;
-	}
-
-	return 0;
-}
+//#include <iostream>
+//#include <string>
+//
+//struct dormitory
+//{
+//	int roomNumb;
+//	float square;
+//	std::string facultName;
+//	int livingIn;
+//};
+//
+//void FacultCountAndWrite(dormitory *arr, int size, std::string* index)		// —чЄтчик уникальных элементов string
+//{
+//	bool unique;
+//
+//	for (int i = 0, count = 0; i < size; i++)
+//	{
+//		unique = true;
+//		for (int j = 0; j < index->size(); j++)
+//		{
+//			if (index[j] == arr[i].facultName)
+//			{
+//				unique = false;
+//			}
+//		}
+//
+//		if (unique)
+//		{
+//			index[count] = arr[i].facultName;
+//			count++;
+//		}
+//	}
+//}
+//
+//int main()
+//{
+//	int size;
+//
+//	std::cin >> size;
+//	dormitory *arr = new dormitory[size];
+//
+//	for (int i = 0; i < size; i++)
+//	{
+//		std::cout << "Room number: ";
+//		std::cin >> arr[i].roomNumb;
+//		std::cout << "Square: ";
+//		std::cin >> arr[i].square;
+//		std::cout << "Name of Facultet: ";
+//		std::cin >> arr[i].facultName;
+//		std::cout << "How many people live in: ";
+//		std::cin >> arr[i].livingIn;
+//	}
+//
+//	std::string* facult = new std::string[size];			// сделать массивом :)
+//	FacultCountAndWrite(arr, size, facult);
+//
+//	int roomcount = 0, studentscount = 0;
+//	float midsquare = 0;
+//	for (int i = 0; i < size; i++)
+//	{ 
+//		std::cout << "Name of facult: " << facult[i] << std::endl;
+//		for (int j = 0; j < size; j++)
+//		{
+//			if (arr[j].facultName == facult[i])
+//			{
+//				roomcount++;
+//				studentscount += arr[j].livingIn;
+//				midsquare += arr[j].square;
+//			}
+//		}
+//
+//		midsquare /= studentscount;
+//
+//		std::cout << "No of rooms: " << roomcount 
+//			<< "  No of students: " << studentscount 
+//			<< "  Mid square: " << midsquare << std::endl << std::endl;
+//
+//		roomcount = 0;
+//		studentscount = 0;
+//		midsquare = 0;
+//	}
+//
+//	return 0;
+//}
