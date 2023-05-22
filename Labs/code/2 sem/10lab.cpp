09f336c5afc7c5f702dee059abfb50c4f8d367ae
KamysDev <sereginka070504@gmail.com>
2023-05-23 01:14:31 +0700

    Added some files

--- Labs/code/2 sem/10lab.cpp
+++ Labs/code/2 sem/10lab.cpp
@@ -1,87 +1,86 @@
-#include <iostream>
-#include <string>
-#include <fstream>
-
-int tempcount = 0;
-
-void selectSortData(std::string* array, int size)
-{
-	for (int step = 0; step < size - 1; ++step) 
-	{
-		for (int i = 0; i < size - step - 1; ++i) 
-		{
-			if (array[i] > array[i + 1])
-			{
-				std::swap<std::string>(array[i], array[i + 1]);
-			}
-		}
-	}
-}
-
-int StringDelimiter(char* temp, std::string* lines, bool IsWrite)
-{
-	char* count[50] = {};
-	char* tmp_char = strtok_s(temp, " ", count);
-
-	while (tmp_char != NULL)
-	{
-		if (IsWrite)
-		{
-			lines[tempcount] = tmp_char;	
-		}
-		tmp_char = strtok_s(NULL, " ", count);
-		tempcount++;
-	}		
-
-	return tempcount;
-}
-
-int main()
-{
-	std::ifstream input("./code/2 sem/materials/input.txt");
-	std::ofstream output("./code/2 sem/materials/output.txt");
-
-	std::string freespace = "";
-	char temp_data[256];
-	int arraysize = 0;
-
-
-	if ((input.is_open() || output.is_open()) == NULL)
-	{
-		std::cout << "Input error is: "
-			<< input.is_open()
-			<< "Output erroe is: "
-			<< output.is_open();
-		return -1;
-	}
-
-	for (int i = 0; input.getline(temp_data, 256); i++)
-	{
-		arraysize += StringDelimiter(temp_data, NULL, NULL);
-	}
-
-	
-	std::string* lines = new std::string[arraysize];
-
-	input.close();
-	input.open("./code/2 sem/materials/input.txt");
-
-	tempcount = 0;			// Relocate first step
-
-	for (int i = 0; input.getline(temp_data, 256); i++)
-	{
-		int ELine = StringDelimiter(temp_data, lines, 1);
-	}
-
-	selectSortData(lines, arraysize);
-
-	for (int i = 0; i < arraysize; i++)
-	{
-		if (lines[i] != freespace)
-		{
-			output << lines[i] << " ";
-		}
-	}
-
-	return 0;
-}
+//#include <iostream>
+//#include <string>
+//#include <fstream>
+//
+//int tempcount = 0;
+//
+//void selectSortData(std::string* array, int size)
+//{
+//	for (int step = 0; step < size - 1; ++step) 
+//	{
+//		for (int i = 0; i < size - step - 1; ++i) 
+//		{
+//			if (array[i] > array[i + 1])
+//			{
+//				std::swap<std::string>(array[i], array[i + 1]);
+//			}
+//		}
+//	}
+//}
+//
+//int StringDelimiter(char* temp, std::string* lines, bool IsWrite)
+//{
+//	char* count[50] = {};
+//	char* tmp_char = strtok_s(temp, " ", count);
+//
+//	while (tmp_char != NULL)
+//	{
+//		if (IsWrite)
+//		{
+//			lines[tempcount] = tmp_char;	
+//		}
+//		tmp_char = strtok_s(NULL, " ", count);
+//		tempcount++;
+//	}		
+//
+//	return tempcount;
+//}
+//
+//int main()
+//{
+//	std::ifstream input("./code/2 sem/materials/input.txt");
+//	std::ofstream output("./code/2 sem/materials/output.txt");
+//
+//	std::string freespace = "";
+//	char temp_data[256];
+//	int arraysize = 0;
+//
+//	if ((input.is_open() || output.is_open()) == NULL)
+//	{
+//		std::cout << "Input error is: "
+//			<< input.is_open()
+//			<< "Output erroe is: "
+//			<< output.is_open();
+//		return -1;
+//	}
+//
+//	for (int i = 0; input.getline(temp_data, 256); i++)
+//	{
+//		arraysize += StringDelimiter(temp_data, NULL, NULL);
+//	}
+//
+//	
+//	std::string* lines = new std::string[arraysize];
+//
+//	input.close();
+//	input.open("./code/2 sem/materials/input.txt");
+//
+//	tempcount = 0;			// Relocate first step
+//
+//	for (int i = 0; input.getline(temp_data, 256); i++)
+//	{
+//		int ELine = StringDelimiter(temp_data, lines, 1);
+//	}
+//
+//	selectSortData(lines, arraysize);
+//
+//	for (int i = 0; i < arraysize; i++)
+//	{
+//		if (lines[i] != freespace)
+//		{
+//			output << lines[i] << " ";
+//		}
+//	}
+//
+//	return 0;
+//}
---
+++ Labs/code/Testing/Lab6_ZFO.cpp
@@ -0,0 +1,188 @@
+﻿//#define _CRT_SECURE_NO_WARNINGS
+//
+//#include <stdio.h>
+//#include <conio.h>
+//#include <string.h>
+//#include <stdlib.h>
+//#include <locale.h>
+//
+//struct Notebook
+//{
+//	char secondName[20];
+//	char phoneNumber[20];
+//	char birthdate[10];
+//
+//	Notebook() = default;
+//} myNotebook;
+//
+//int input(const char* name)
+//{
+//	char ch;
+//	FILE* tf;
+//	tf = fopen(name, "wb");
+//	if (tf == NULL)
+//	{
+//		return -1;
+//	}
+//	system("CLS");
+//	printf("\n Entering a contact \n");
+//	do
+//	{
+//		printf("\n Surname: "); scanf("%s", myNotebook.secondName);
+//		printf(" Phone number: "); scanf("%s", myNotebook.phoneNumber);
+//		printf(" Date of birth: "); scanf("%s", myNotebook.birthdate);
+//		fwrite(&myNotebook, sizeof(myNotebook), 1, tf); // запись в файл одной структуры myNotebook
+//		printf("\n End?  y/n  ");
+//		ch = _getch();
+//	} while (ch != 'y');
+//	fclose(tf);
+//
+//	return 0;
+//}
+//
+//int print(const char* name) // просмотр файла
+//{
+//	int i; FILE* tf;
+//	system("CLS");
+//	tf = fopen(name, "rb"); // открытие бинарного файла для чтения
+//	if (tf == NULL)
+//	{
+//		return -1;
+//	}
+//	i = 1;
+//
+//	for (int i = 0; fread(&myNotebook, sizeof(myNotebook), 1, tf) == 1; i++)
+//	{
+//		printf("\n  %3d Second name  %20s, Phone number %20s, birthdate %10s \n",
+//			i,
+//			myNotebook.secondName,
+//			myNotebook.phoneNumber,
+//			myNotebook.birthdate);
+//	}
+//
+//	system("Pause");
+//
+//	fclose(tf);
+//
+//	return 0;
+//}
+//
+//int app(const char* name) // добавление в файл
+//{
+//	FILE* tf;
+//
+//	char ch{};
+//	char* str{};
+//	char* final_str{};
+//
+//	tf = fopen(name, "ab"); // открытие бинарного файла для добавления
+//	if (tf == NULL)
+//	{
+//		return -1;
+//	}
+//	system("CLS");
+//	printf("\n  Entering a contact \n");
+//
+//	printf("\n Surname: "); scanf("%s", myNotebook.secondName);
+//	printf(" Phone number: "); scanf("%s", myNotebook.phoneNumber);
+//	printf(" Date of birth: "); scanf("%s", myNotebook.birthdate);
+//
+//	char c{};
+//	while ((c = getc(tf)) != EOF)
+//	{
+//		*final_str += c;
+//	}
+//
+//	fseek(tf, 0, SEEK_SET);
+//	fwrite(&myNotebook, sizeof(myNotebook), 1, tf);
+//	fwrite(&final_str, sizeof(final_str), 1, tf);
+//
+//	fclose(tf);
+//
+//	return 0;
+//}
+//
+//int find(const char* name)
+//{
+//	char c{}, Notebook[10]{}; FILE* tf;
+//	tf = fopen(name, "rb+");
+//	if (tf == NULL)
+//	{
+//		return -1;
+//	}
+//
+//	printf("Product to search for: ");
+//	scanf("%s", &Notebook);
+//	fread(&myNotebook, sizeof(myNotebook), 1, tf);
+//
+//	while (true)
+//	{
+//		if (strcmp(myNotebook.secondName, Notebook) != 1)
+//		{
+//			system("CLS");
+//			printf("\nSecond name %s Phone number %s Birthdate %s",
+//				myNotebook.secondName,
+//				myNotebook.phoneNumber,
+//				myNotebook.birthdate);
+//
+//			printf("\n Continue? y/n ");
+//			if (_getch() == 'y')
+//			{
+//				printf("\n Phone number: ");
+//				scanf("%s", myNotebook.phoneNumber);
+//				printf("\n Birthdate: ");
+//				scanf("%s", myNotebook.birthdate);
+//
+//				fseek(tf, sizeof(myNotebook), 1);
+//				fwrite(&myNotebook, sizeof(myNotebook), 1, tf);
+//				fflush(tf);
+//				break;
+//			}
+//		}
+//		fread(&myNotebook, sizeof(myNotebook), 1, tf);
+//
+//		if (feof(tf) == 1)
+//		{
+//			break;
+//		}
+//	}
+//	fclose(tf);
+//
+//	return 0;
+//}
+//
+//int main()
+//{
+//	while (1)
+//	{
+//		system("CLS");
+//		printf(" 1 - creating a new file\n");
+//		printf(" 2 - viewing a file\n");
+//		printf(" 3 - adding to a file\n");
+//		printf(" 4 - search and change\n");
+//		printf(" 0 - exit\n");
+//
+//
+//		switch (_getch())
+//		{
+//		case '1':
+//			input("file1.dat");
+//			break;
+//		case '2':
+//			print("file1.dat");
+//			break;
+//		case '3':
+//			app("file1.dat");
+//			break;
+//		case '4':
+//			find("file1.dat"); 
+//			break;
+//		case '0':
+//			return 0;
+//		default: 
+//			break;
+//		}
+//	}
+//
+//	return 0;
+//}
